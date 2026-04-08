e8b2c4aa936b1a6e44ae4dbf11b4133a0e79e23b
翟二远 <zhaieryuan2022@163.com>
2026-04-08 08:28:52 +0800

    feat : 调整web工程映射的字路径

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import * as path from 'path'
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-  test: {
-    exclude: ['**/node_modules/**', '**/e2e/**'],
   },
   server: {
     port: 3000,
